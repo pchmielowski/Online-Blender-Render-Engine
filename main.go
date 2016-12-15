24206f2150f0c9a07879bdf678726d7c0da4c51a
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-12-15 18:43:48 +0100

    Server refactored

--- main.go
+++ main.go
@@ -1,50 +1,67 @@
 package main
 
 import (
+  "os"
   "os/exec"
   "net/http"
   "html/template"
-  "os"
   "io"
   "io/ioutil"
 )
+
+func main() {
+  http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/render/", renderHandler)
+  http.HandleFunc("/result/", resultHandler)
+  http.ListenAndServe(":8080", nil)
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   http.ServeFile(w, r, "/index.html")
 }
 
 func resultHandler(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "/out0050.png")
+  name := "/out0050.png"
+  http.ServeFile(w, r, name)
 }
 
-func main() {
-  http.HandleFunc("/", indexHandler)
-  http.HandleFunc("/render/", handler)
-  http.HandleFunc("/result/", resultHandler)
-  http.ListenAndServe(":8080", nil)
+func renderHandler(w http.ResponseWriter, r *http.Request) {
+  saveModel(r)
+  render()
+  showResult(w)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  out, err := os.Create("/model.blend")
+func saveModel(r *http.Request) {
+  modelFile, err := os.Create("/model.blend")
   if err != nil {
     panic("error creating")
   }
-  defer out.Close()
+  defer modelFile.Close()
   r.ParseMultipartForm(32 << 20)
   file, _, err := r.FormFile("file")
   if err != nil {
     panic("error parsing form")
   }
   defer file.Close()
-  io.Copy(out, file)
+  io.Copy(modelFile, file)
+}
 
-  cmd := exec.Command("/bin/bash", "/run.bash")
-  err1 := cmd.Run()
-  if err1 != nil {
-    panic(err1)
+func showResult(w http.ResponseWriter) {
+  time, timeErr := ioutil.ReadFile("/time")
+  if timeErr != nil {
+    panic(timeErr)
   }
+  templ, templErr := template.ParseFiles("/result.html")
+  if templErr != nil {
+    panic(templErr)
+  }
+  templ.Execute(w, string(time))
+}
 
-  time, _ := ioutil.ReadFile("/time")
-  t, _ := template.ParseFiles("/result.html")
-  t.Execute(w, string(time))
+func render() {
+  command := exec.Command("/bin/bash", "/run.bash")
+  err := command.Run()
+  if err != nil {
+    panic(err)
+  }
 }
